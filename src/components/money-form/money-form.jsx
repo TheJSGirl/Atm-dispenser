4634d4fccda20bb0da6e37457c982a2f471b42b6
India Hiring <prinegi100@gmail.com>
2019-07-24 14:23:18 +0530

    feat: added css & events for money-form

--- src/components/money-form/money-form.jsx
+++ src/components/money-form/money-form.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import './money-form.css';
 
 class MoneyForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            money: ''
+        }
+        this.handleOnchange = this.handleOnchange.bind(this);
+    }
+
+    handleOnchange(event) {
+        event.preventDefault();
+        this.setState({ money: event.target.value })
+    }
     render() {
         return (
-            <div>
-                <h2>Welcome to ATM</h2>
+            <div className="money-form">
+                <div  >
+                    <h1 className="form-heading">Welcome to ATM</h1>
+                    <div>
+                        <div> <label>Enter Amount</label></div>
+                        <input
+                            className="form-input"
+                            type="text"
+                            value={this.state.money}
+                            name="money"
+                            onChange={this.handleOnchange}
+
+                        />
+                    </div>
+
+                    <div className="form-btn">
+                        <button className="btn">Get Money</button>
+                    </div>
+                </div>
             </div>
         )
     }
