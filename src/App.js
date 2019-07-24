161dd289b0b473259c8fba9ea6cc8141eab43482
India Hiring <prinegi100@gmail.com>
2019-07-24 13:52:08 +0530

    feat: added heading & initiated money-form component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import MoneyForm from './components/money-form/money-form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <h1>ATM Money Dispenser </h1>
+      </div>
+      <MoneyForm />
     </div>
   );
 }
