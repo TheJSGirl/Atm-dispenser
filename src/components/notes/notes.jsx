ab28d1ca7556b1deb4e1e1095924930641970812
thejsgirl <prinegi100@gmail.com>
2019-07-26 20:03:26 +0530

    feat: getNotes()

--- src/components/notes/notes.jsx
+++ src/components/notes/notes.jsx
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import "./notes.css";
 
-class Notes extends Component {
-    render() {
-        const notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
-        const { money } = this.props;
-        const newNote = notes.map((note) => {
-            if (money === note) {
-                return <li>Number. of notes is: {money} </li>
-            }
-        })
+const notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
 
+class Notes extends Component {
+
+
+    getNotes = (money) => {
+       let currentMoney = money;
+       let notesMap = {};
+       let count = 0;
+       for(let a = 0; a < notes.length; a++ ){
+           
+            notesMap[notes[a]] = parseInt(currentMoney / notes[a], 10);
+            count += notesMap[notes[a]]; 
+            currentMoney = currentMoney % notes[a];
+       }
+
+       return { total: count, data: notesMap };
+    }
+
+    render() {
+        const {money} =  this.props;
+        let {total, data} = this.getNotes(money);
         return (
             <React.Fragment>
                 <h2 className="note-heading">You will get the following amount</h2>
                 <div>
                     <ul>
-                        <li>{newNote}</li>
+                        <li>Total note despensed: {total}</li>
                     </ul>
                 </div>
+              <div>{JSON.stringify(data)}</div>
             </React.Fragment>
         )
     }
